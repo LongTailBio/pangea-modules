4ef29d69ab9893e0ab032f89e42688cb94aeecba
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2018-11-07 15:47:48 -0500

    Remove deprecated kraken tool.

--- modules/kraken/pangea_modules/kraken/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Kraken is an ultrafast and highly accurate program for assigning
-taxonomic labels to metagenomic DNA sequences.
-"""
-
-from .modules import KrakenResultModule, MODULE_NAME
-from .models import KrakenResult
