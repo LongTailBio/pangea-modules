e8c7768737a9fe207dd2369a3f4d4bbda24075c8
David Danko <dcdanko@gmail.com>
2018-11-08 14:22:03 -0500

    add matrix tests

--- modules/base/tests/test_data_tensor_models.py
+++ modules/base/tests/test_data_tensor_models.py
@@ -5,9 +5,11 @@ from unittest import TestCase
 
 from pangea_modules.base.data_tensors import (
     Vector,
+    Matrix,
 )
 from pangea_modules.base.data_tensor_models import (
     VectorModel,
+    MatrixModel,
 )
 
 
@@ -37,3 +39,20 @@ class TestDataTensorModels(TestCase):
         unindexed_vector = unindexed_vector_model.from_son([1, 2])
         self.assertIs(type(unindexed_vector), Vector)
         self.assertFalse(unindexed_vector.indexed)
+
+    def test_indexed_matrix_produces_correct_schema(self):
+        """Ensure an indexed matrix produces the correct mongoengine type."""
+        indexed_matrix_model = MatrixModel(float, indexed=True)
+        self.assertIs(type(indexed_matrix_model.get_document_class()), mdb.MapField)
+
+    def test_indexed_matrix_from_son(self):
+        """Ensure an indexed matrix builds the correct type from a SON blob."""
+        data = {
+            'col_1': {'row_1': 1, 'row_2': 2},
+            'col_2': {'row_1': 3, 'row_2': 4},
+        }
+        indexed_matrix_model = MatrixModel(float, indexed=True)
+        indexed_matrix = indexed_matrix_model.from_son(data)
+        self.assertIs(type(indexed_matrix), Matrix)
+        self.assertTrue(indexed_matrix.col_indexed)
+        self.assertTrue(indexed_matrix.row_indexed)
