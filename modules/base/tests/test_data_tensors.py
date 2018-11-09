3eff394bdb8acfc6004de32064c97d1b324cba50
David Danko <dcdanko@gmail.com>
2018-11-09 10:26:38 -0500

    fixed more linting bugs

--- modules/base/tests/test_data_tensors.py
+++ modules/base/tests/test_data_tensors.py
@@ -12,13 +12,6 @@ from pangea_modules.data_tensors import (
 class TestDataTensors(TestCase):
     """Test suite for Data Tensors."""
 
-    def test_scalar_group_retrieval(self):
-        """Ensure scalar group retrieval works correctly."""
-        data = {'a': 1, 'b': 2}
-        scalar_group = ScalarGroup(**data)
-        for key, val in data.items():
-            self.assertEqual(scalar_group[key], val)
-
     def test_indexed_vector_data_retrieval(self):
         """Ensure an indexed vector retrieves data correctly."""
         data = {'a': 1, 'b': 2}
