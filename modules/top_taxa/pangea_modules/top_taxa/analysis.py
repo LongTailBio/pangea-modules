a1ace6605891dd7e978f6b17f8ccc2cc79fee1cf
David Danko <dcdanko@gmail.com>
2018-11-20 11:15:06 -0500

    fix top taxa tests and linting

--- modules/top_taxa/pangea_modules/top_taxa/analysis.py
+++ modules/top_taxa/pangea_modules/top_taxa/analysis.py
@@ -1,7 +1,5 @@
 """Tasks for generating Top Taxa Size results."""
 
-from pandas import DataFrame
-
 from pangea_modules.base.utils import group_samples_by_metadata
 from pangea_modules.krakenhll_data import KrakenHLLResultModule
 from pangea_modules.metaphlan2_data import Metaphlan2ResultModule
@@ -25,7 +23,7 @@ def processor(samples):
         out = {}
         for tool in [KRAKENHLL, METAPHLAN]:
             out[tool.name()] = {}
-            taxa_matrix = tool.promote(my_samples)['taxa'].as_compositional()
+            taxa_matrix = tool.promote_data(my_samples)['taxa'].as_compositional()
             for kingdom in ['all_kingdoms']:
                 kingdom_taxa_matrix = filter_taxa_by_kingdom(taxa_matrix, kingdom)
                 out[tool.name()][kingdom] = {
@@ -34,5 +32,5 @@ def processor(samples):
                 }
         return out
 
-    _, taxa_vecs = group_samples_by_metadata(samples, group_apply=group_apply)
+    _, top_taxa = group_samples_by_metadata(samples, group_apply=group_apply)
     return {'categories': top_taxa}
