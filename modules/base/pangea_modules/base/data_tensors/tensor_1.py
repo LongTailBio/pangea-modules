9fd31380b52cc57a4cda90da6887366540aeb317
David Danko <dcdanko@gmail.com>
2018-11-21 14:49:47 -0500

    reduce subclassing

--- modules/base/pangea_modules/base/data_tensors/tensor_1.py
+++ modules/base/pangea_modules/base/data_tensors/tensor_1.py
@@ -1,8 +1,8 @@
 """Represent tensors that contain atomic tensors."""
 
-import pandas as pd
+import numpy as np
 
-from .vector_data_processing import VectorProcessing
+from .vector_data_access import VectorAccess
 
 
 class Tensor1:  # pylint: disable=too-few-public-methods
@@ -21,8 +21,23 @@ class ScalarGroup(Tensor1):  # pylint: disable=too-few-public-methods
         return self.scalars[key]
 
 
-class Vector(VectorProcessing, Tensor1):
+class Vector(VectorAccess, Tensor1):
     """Represent a sequence of numerical scalars."""
 
-    def __init__(self, data):
-        self.data = pd.Series(data)
+    def as_compositional(self):
+        """Return a vector proportional to this one that sums to 1."""
+        my_sum = self.sum()
+        return self.operate(lambda val: val / my_sum)
+
+    def percentile(self, *percentiles):
+        """Return a vector of percentiles of the data."""
+        return type(self)(np.percentile(self.values, percentiles))  # pylint: disable=no-member
+
+    def quartiles(self):
+        """Return a Vector of length 5 for 0, 25, 50, 75, 100 percentiles."""
+        return self.percentile(0, 25, 50, 75, 100)
+
+    def num_non_zero(self, zero_thresh=0.00000000001):
+        """Return the number of elements with value larger than <zero_thresh>."""
+        vals = [val for _, val in self.items() if abs(val) < zero_thresh]
+        return len(vals)
--- modules/base/pangea_modules/base/data_tensors/vector_data_access.py
+++ modules/base/pangea_modules/base/data_tensors/vector_data_access.py
@@ -6,26 +6,19 @@ import pandas as pd
 class VectorAccess(pd.Series):
     """Represent a sequence of numerical scalars."""
 
-    data = pd.Series()
-
     def __getattribute__(self, key):
-        my_attr = getattr(self.data, key)
+        my_attr = getattr(self, key)
 
         def wrapit(*args, **kwargs):
             """Intercept calls to pandas functions and conver to vectors."""
             pd_val = my_attr(*args, **kwargs)
+            if isinstance(pd_val, type(self)):
+                return pd_val
             if isinstance(pd_val, pd.Series):
                 return type(self)(pd_val)
             return pd_val
 
         return wrapit
-
-    def __getitem__(self, key):
-        return self.data[key]
-
-    def to_pandas(self):
-        """Return a pandas series based on this vector."""
-        return self.data
 
     def operate(self, operator):
         """Return a vector with <operator> applied to each element."""
