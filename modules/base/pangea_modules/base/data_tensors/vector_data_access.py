378119c6b9300a1455b5a2f31fbe6b9e6273a661
David Danko <dcdanko@gmail.com>
2018-11-21 14:12:11 -0500

    Aggressively convert vector to duck typed pandas series.

--- modules/base/pangea_modules/base/data_tensors/vector_data_access.py
+++ modules/base/pangea_modules/base/data_tensors/vector_data_access.py
@@ -6,35 +6,37 @@ import pandas as pd
 class VectorAccess:
     """Represent a sequence of numerical scalars."""
 
-    data = {}
+    data = pd.Series()
+
+    def __getattr__(self, key):
+        try:
+            pd_attr = self.data.key
+
+            def wrapit(*args, **kwargs):
+                """Intercept calls to pandas functions and conver to vectors."""
+                pd_val = pd_attr(*args, **kwargs)
+                if isinstance(pd_val, pd.Series):
+                    return type(self)(pd_val)
+                return pd_val
+
+            return wrapit
+        except AttributeError:
+            raise
 
     def __getitem__(self, key):
         return self.data[key]
 
-    def __len__(self):
-        """Return the length of this vector."""
-        return len(self.data)
-
-    def as_pandas(self):
+    def to_pandas(self):
         """Return a pandas series based on this vector."""
-        return pd.Series.from_dict({str(key): val for key, val in self.data.items()})
-
-    def as_dict(self):
-        """Return this vector as a dict."""
         return self.data
-
-    def iter(self):
-        """Yield tuples of key, value."""
-        for key, val in self.data.items():
-            yield key, val
 
     def operate(self, operator):
         """Return a vector with <operator> applied to each element."""
-        return type(self)({key: operator(val) for key, val in self.iter()})
+        return type(self)({key: operator(val) for key, val in self.items()})
 
     def reduce(self, operator):
         """Return the function applied to the values in the vector.
 
         Order of values is not guaranteed.
         """
-        return operator(list(self.data.values()))
+        return operator(self.get_values())
